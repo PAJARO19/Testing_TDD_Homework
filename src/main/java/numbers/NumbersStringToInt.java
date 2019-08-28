8afc9d8a94aa2841f7d991c6ae839060f3674a62
PAJARO19 <fiolek0301@tlen.pl>
2019-08-28 18:06:06 +0200

    changes to NumbersStringToInt after code review

--- src/main/java/numbers/NumbersStringToInt.java
+++ src/main/java/numbers/NumbersStringToInt.java
@@ -1,48 +1,36 @@
 package numbers;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NumbersStringToInt {
 
     public Integer stringToInt(String string) {
         Integer result = 0;
-        List<Character> list = Arrays.asList('I', 'V', 'X', 'L', 'C', 'D', 'M');
-        for (int i = 0; i <= string.length() - 1; i++) {
-            if (!(string.charAt(i) == list.get(0)
-                    || string.charAt(i) == list.get(1)
-                    || string.charAt(i) == list.get(2)
-                    || string.charAt(i) == list.get(3)
-                    || string.charAt(i) == list.get(4)
-                    || string.charAt(i) == list.get(5)
-                    || string.charAt(i) == list.get(6)))
-                result = null;
+        char[] stringToCharTable = string.toCharArray();
+        Map<Character, Integer> map1 = new HashMap<>();
+        map1.put('I', 1);
+        map1.put('V', 5);
+        map1.put('X', 10);
+        map1.put('L', 50);
+        map1.put('C', 100);
+        map1.put('D', 500);
+        map1.put('M', 1000);
+        for (char a : stringToCharTable) {
+            if (!map1.containsKey(a))
+                return null;
         }
-        if (result != null) {
-            for (int i = 0; i <= string.length() - 1; i++) {
-                if (string.charAt(i) == 'I')
-                    result += 1;
-                if (string.charAt(i) == 'V')
-                    result += 5;
-                if (string.charAt(i) == 'X')
-                    result += 10;
-                if (string.charAt(i) == 'L')
-                    result += 50;
-                if (string.charAt(i) == 'C')
-                    result += 100;
-                if (string.charAt(i) == 'D')
-                    result += 500;
-                if (string.charAt(i) == 'M')
-                    result += 1000;
-            }
-            for (int i = 0; i <= string.length() - 2; i++) {
-                if (string.charAt(i) == 'I' && (string.charAt(i + 1) == 'V' || string.charAt(i + 1) == 'X'))
-                    result -= 2;
-                if (string.charAt(i) == 'X' && (string.charAt(i + 1) == 'L' || string.charAt(i + 1) == 'C'))
-                    result -= 20;
-                if (string.charAt(i) == 'C' && (string.charAt(i + 1) == 'D' || string.charAt(i + 1) == 'M'))
-                    result -= 200;
-            }
+        for (char a : stringToCharTable) {
+            if (map1.containsKey(a))
+                result += map1.get(a);
+        }
+        for (int i = 0; i <= stringToCharTable.length - 2; i++) {
+            if (stringToCharTable[i] == 'I' && (stringToCharTable[i + 1] == 'V' || stringToCharTable[i + 1] == 'X'))
+                result -= 2;
+            if (stringToCharTable[i] == 'X' && (stringToCharTable[i + 1] == 'L' || stringToCharTable[i + 1] == 'C'))
+                result -= 20;
+            if (stringToCharTable[i] == 'C' && (stringToCharTable[i + 1] == 'D' || stringToCharTable[i + 1] == 'M'))
+                result -= 200;
         }
         return result;
     }
